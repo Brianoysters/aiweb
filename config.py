f72aff8f553371b3dac50410b796cbbcf2da9ece
Brianoysters <144411003+Brianoysters@users.noreply.github.com>
2025-04-20 18:40:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ load_dotenv()
 
 class Config:
     # Database Configuration
-    DATABASE_URL = "mysql://root:RlnjaHZoFYoaoxssxFHKtLFQlvwqninP@yamanote.proxy.rlwy.net:17657/railway"
+    # old mysql://root:hHqxqLIMvuNqKHjzAPNwVpgqJitEJhrM@gondola.proxy.rlwy.net:41520/railway
+    DATABASE_URL = "mysql://root:hHqxqLIMvuNqKHjzAPNwVpgqJitEJhrM@gondola.proxy.rlwy.net:41520/railway"
+    #DATABASE_URL = "mysql://root:RlnjaHZoFYoaoxssxFHKtLFQlvwqninP@yamanote.proxy.rlwy.net:17657/railway"
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
